ea19c4ab92b2398a96d2a1a585c5d4da8b2ef379
dangowans <d.gowans@cityssm.on.ca>
2021-03-02 15:32:12 -0500

    development

--- data/itemImages.ts
+++ data/itemImages.ts
@@ -9,25 +9,31 @@ export const itemImages: configTypes.Config_ItemImages = {
   "BATTERIES-LARGE": "batteries-large.jpg",
   "BATTERIES-SMALL": "batteries.jpg",
   BLANKETS: "blankets.jpg",
+  BLURAY: "compact-discs.jpg",
   BOOKS: "books.jpg",
   "BOTTLES-BEER": "bottles-beer.jpg",
   "BOTTLES-GLASS": "bottles-glass.jpg",
-  "BOTTLES-PLASTIC-1": "bottles-plastic-1.jpg",
+  "BOTTLES-WINE": "bottles-wine.jpg",
 
   CANS: "cans-food.jpg",
   "CANS-BEER": "cans-beer.jpg",
+  "CAR-PARTS": "car-parts.jpg",
   CARS: "car.jpg",
   CIGARETTES: "cigarettes.jpg",
   "CLOTHING-GENTLYUSED": "clothing.jpg",
   CUTLERY: "cutlery.jpg",
 
   DISHES: "dishes.jpg",
+  DVDS: "compact-discs.jpg",
 
   ELECTRONICS: "computers.jpg",
   "ELECTRONICS-GAME-CONSOLES": "xbox.jpg",
   "ELECTRONICS-SMARTPHONE": "smartphone.jpg",
 
   FABRIC: "fabric.jpg",
+  "FURNITURE-BEDROOM": "furniture-bedroom.jpg",
+  "FURNITURE-DININGROOM": "furniture-diningroom.jpg",
+  "FURNITURE-LIVINGROOM": "furniture-livingroom.jpg",
 
   "INK-CARTRIDGES": "ink-cartridges.jpg",
 
@@ -40,13 +46,17 @@ export const itemImages: configTypes.Config_ItemImages = {
 
   NEEDLES: "needles.jpg",
 
+  PAPER: "paper.jpg",
   "PAPER-CARDBOARD": "cardboard.jpg",
   "PAPER-MAGAZINES": "magazines.jpg",
+  "PET-SUPPLIES": "pet-supplies.jpg",
   PILLOWS: "pillows.jpg",
   "PIPES-METAL": "pipe-metal.jpg",
+  "PLASTIC-1": "bottles-plastic-1.jpg",
   PURSES: "purse.jpg",
 
   "SHOES-GENTLYUSED": "shoes.jpg",
+  "STYROFOAM-TRAYS": "styrofoam-tray.jpg",
   "SPORTS-HOCKEY": "hockey.jpg",
 
   "TIRES-CAR": "tires-car.jpg",
